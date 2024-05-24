225aa1b59bed3622cef9ed262c1e836bb63386cb
hyungmin <hamhmin1031@gmail.com>
2024-05-24 17:15:50 +0900

    2

---
+++ src/main.js
@@ -0,0 +1,80 @@
+import {cm1,cm2} from "./js/common"
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+// ----- 주제: The Bridge 게임 만들기
+
+// Renderer
+// const canvas = document.querySelector('#three-canvas');
+const renderer = new THREE.WebGLRenderer({
+	canvas:cm1.canvas,
+	antialias: true
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+//Scene은 common.js 에서 생성함 cm1.scene
+cm1.scene.background = new THREE.Color(cm2.backgroundColor)
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
+);
+camera.position.x = -4;
+camera.position.y = 19;
+camera.position.z = 14;
+cm1.scene.add(camera);
+
+// Light
+const ambientLight = new THREE.AmbientLight(cm2.lightColor, 0.8);
+cm1.scene.add(ambientLight);
+
+const spotLightDistance = 50;
+const spotLight1 = new THREE.SpotLight(cm2.lightColor,1);
+spotLight1.castShadow = true;
+const spotLight2 = spotLight1.clone();
+const spotLight3 = spotLight1.clone();
+const spotLight4 = spotLight1.clone();
+spotLight1.position.set(-spotLightDistance,spotLightDistance,spotLightDistance);
+spotLight2.position.set(spotLightDistance,spotLightDistance,spotLightDistance);
+spotLight3.position.set(-spotLightDistance,spotLightDistance,-spotLightDistance);
+spotLight4.position.set(-spotLightDistance,spotLightDistance,-spotLightDistance);
+cm1.scene.add(spotLight1,spotLight2,spotLight3,spotLight4)
+
+// Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+
+// 바닥Mesh
+
+
+
+// 그리기
+const clock = new THREE.Clock();
+
+function draw() {
+	const delta = clock.getDelta();
+
+	controls.update();
+
+	renderer.render(cm1.scene, camera);
+	renderer.setAnimationLoop(draw);
+}
+
+function setSize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.render(cm1.scene, camera);
+}
+
+// 이벤트
+window.addEventListener('resize', setSize);
+
+draw();
